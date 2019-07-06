479215055cc7cf5b45a826bbf46e7cdaa1c77a93
Douglas Newquist <deathsnake@protonmail.com>
2019-07-05 19:57:53 -0600

    Moved Project files into root folder

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class Entity : MonoBehaviour
+{
+	public Health health = new Health();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (health.IsDead)
+		{
+			gameObject.SetActive(false);
+			OnDeath();
+		}
+    }
+
+	/// <summary>
+	/// Called when this entity is killed
+	/// </summary>
+	protected virtual void OnDeath() { }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Health
+{
+	[Range(0, 1000)]
+	[SerializeField]
+	[Tooltip("The base HP of this entity")]
+	private int baseHealth = 100;
+
+	[SerializeField]
+	private int max;
+
+	/// <summary>
+	/// The maximum amount of hp for this entity
+	/// </summary>
+	public int Max
+	{
+		get { return max; }
+		set { max = value; }
+	}
+
+	[SerializeField]
+	private int current;
+
+	/// <summary>
+	/// The current amount of hp remaining
+	/// </summary>
+	public int Current
+	{
+		get { return current; }
+		set { current = value; }
+	}
+
+	/// <summary>
+	/// Returns true if this entity has zero health left
+	/// </summary>
+	public bool IsDead { get { return current == 0; } }
+
+	public Health()
+	{
+		max = baseHealth;
+	}
+
+	public Health(int baseHealth)
+	{
+		this.baseHealth = baseHealth;
+		max = baseHealth;
+	}
+
+	public void Reset()
+	{
+		current = max;
+	}
+
+	public void ApplyDamage(int amount)
+	{
+		current = Mathf.Clamp(current - amount, 0, max);
+	}
+
+	public void Heal(int amount)
+	{
+		ApplyDamage(-amount);
+	}
+}
