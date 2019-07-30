1e5d66b6581690b22ca941d4379e292695a88d3c
Douglas Newquist <deathsnake@protonmail.com>
2019-07-30 15:10:26 -0600

    Added player attacks

---
+++ Assets/Scripts/TargetCursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCursor : MonoBehaviour
+{
+	public bool targetMouse = true;
+
+	public Vector3 target;
+
+	public float angleOffset = 90;
+
+    void Update()
+    {
+		if (targetMouse)
+		{
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = 0;
+		}
+
+		var d = target - transform.position;
+		float angle = Mathf.Atan2(d.y, d.x) * 180f / Mathf.PI;
+		transform.localRotation = Quaternion.Euler(0, 0, angle + angleOffset);
+	}
+}
